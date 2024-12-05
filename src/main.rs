3f5bdfd67a0ed788aa74a3d90ae52949eee57e12
Sam Baskin <sambskn@gmail.com>
2024-12-04 23:12:18 -0700

    working day 5 lets gooo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::char;
 use std::env;
 use std::fs::File;
 use std::io;
@@ -17,74 +16,101 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
 
-    // setup cells
-    let mut cells: Vec<Vec<i8>> = vec![];
+    let mut reading_rules = true;
+
+    let mut rules: Vec<Rule> = vec![];
+    let mut valid_orderings: Vec<Vec<String>> = vec![];
+    let mut fixed_orderings: Vec<Vec<String>> = vec![];
 
     println!("start!");
     if let Ok(lines) = read_lines_from_file(file_path) {
         for line in lines.flatten() {
-            let mut row: Vec<i8> = vec![];
-            for letter in line.chars() {
-                // give each cell a value based on word we're looking for
-                row.push(match letter {
-                    'A' => 1, // center of target
-                    'M' => 2, // need to find 2
-                    'S' => 3, // need to find 2
-                    _ => 0,
-                })
+            let line_len = line.len();
+            if line_len == 0 {
+                reading_rules = false;
+            } else {
+                if reading_rules {
+                    // handle ordering rule
+                    let parts: Vec<&str> = line.split("|").collect();
+                    rules.push(Rule {
+                        before: parts[0].to_string(),
+                        after: parts[1].to_string(),
+                    });
+                } else {
+                    // hanlde ordering
+                    let parts: Vec<&str> = line.split(",").collect();
+                    let mut ordering: Vec<String> = vec![];
+                    for part in parts {
+                        ordering.push(part.to_string());
+                    }
+                    if is_valid(&ordering, &rules) {
+                        valid_orderings.push(ordering);
+                    } else {
+                        fixed_orderings.push(fix_ordering(&ordering, &rules))
+                    }
+                }
             }
-            cells.push(row);
         }
-    }
-    let mut match_count = 0;
-    let height = cells.len();
-    let width = cells[0].len();
-    for row in 0..cells.len() {
-        for col in 0..cells[row].len() {
-            let cell_val = cells[row][col];
-            // check every possible direction for match, if it's the start
-            // print!("{cell_val}");
-            if cell_val == 1 {
-                match_count += find_matches_for_cell(&cells, &col, &row, &width, &height)
-            }
+        let valid_count = valid_orderings.len();
+        println!("valid count: {valid_count}");
+
+        // get middle vals
+        let mut total = 0;
+        for ordering in valid_orderings.iter() {
+            let ordering_middle = (ordering.len() - 1) / 2;
+            let num: u32 = ordering[ordering_middle].parse().unwrap();
+            total += num;
         }
-        // println!("");
+        println!("total of middle pages: {total}");
+
+        total = 0;
+        for ordering in fixed_orderings.iter() {
+            let ordering_middle = (ordering.len() - 1) / 2;
+            let num: u32 = ordering[ordering_middle].parse().unwrap();
+            total += num;
+        }
+        println!("total of middle pages: {total}");
     }
-    println!("total match count: {match_count}")
 }
 
-// only run for the start of matches plz
-pub fn find_matches_for_cell(
-    cells: &Vec<Vec<i8>>,
-    x: &usize,
-    y: &usize,
-    width: &usize,
-    height: &usize,
-) -> i32 {
-    let mut match_count = 0;
+pub struct Rule {
+    pub before: String,
+    pub after: String,
+}
 
-    // check every possible direction for a match
-
-    // needs to be off the edge by one
-    if *x > 0 && *y > 0 && *x < (width - 1) && *y < (height - 1) {
-        // need to find 2 M's (2's) that are on the same 'side' as each other
-        // same for S's (3's)
-        // check upper left cell and go from there
-        let upper_left = cells[y - 1][x - 1];
-        let upper_right = cells[y - 1][x + 1];
-        let lower_left = cells[y + 1][x - 1];
-        let lower_right = cells[y + 1][x + 1];
-        if upper_right == lower_right && upper_left == lower_left {
-            if (upper_right == 2 && upper_left == 3) || (upper_right == 3 && upper_left == 2) {
-                match_count += 1;
-            }
-        }
-        if upper_right == upper_left && lower_right == lower_left {
-            if (upper_right == 2 && lower_right == 3) || (upper_right == 3 && lower_right == 2) {
-                match_count += 1;
+pub fn is_valid(ordering: &Vec<String>, rules: &Vec<Rule>) -> bool {
+    for idx in 0..ordering.len() {
+        for rule in rules.iter() {
+            if rule.after == ordering[idx] {
+                // check if there's any val after thats supposed to be before
+                for sub_idx in (idx + 1)..ordering.len() {
+                    if ordering[sub_idx] == rule.before {
+                        return false;
+                    }
+                }
             }
         }
     }
+    true
+}
 
-    match_count
+pub fn fix_ordering(ordering: &Vec<String>, rules: &Vec<Rule>) -> Vec<String> {
+    let mut new_ordering = ordering.clone();
+    while !is_valid(&new_ordering, rules) {
+        for idx in 0..ordering.len() {
+            for rule in rules.iter() {
+                if rule.after == new_ordering[idx] {
+                    // check if there's any val after thats supposed to be before
+                    for sub_idx in (idx + 1)..ordering.len() {
+                        if new_ordering[sub_idx] == rule.before {
+                            println!("broken rule before {} after {}", rule.before, rule.after);
+                            let popped = new_ordering.remove(idx);
+                            new_ordering.push(popped);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    new_ordering
 }
