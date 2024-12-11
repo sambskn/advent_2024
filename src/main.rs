b99d57c22ca4a6f1b6fadde155c430f60c0a058d
Sam Baskin <sambskn@gmail.com>
2024-12-10 23:11:25 -0700

    day 7 in progress

--- src/main.rs
+++ src/main.rs
@@ -1,222 +1,217 @@
-use std::collections::HashSet;
 use std::env;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
+use std::time::Duration;
 
-fn read_lines_from_file<P>(file_path: P) -> io::Result<io::Lines<io::BufReader<File>>>
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Gauge, Paragraph, Widget},
+    DefaultTerminal, Frame,
+};
+
+fn read_lines_from_file<P>(file_path: P) -> io::Result<Vec<String>>
 where
     P: AsRef<Path>,
 {
     let file = File::open(file_path)?;
-    Ok(io::BufReader::new(file).lines())
+    let lines = io::BufReader::new(file).lines();
+    let mut out = vec![];
+    for line in lines.flatten() {
+        out.push(line);
+    }
+    Ok(out)
 }
 
-fn main() {
+#[derive(Debug, PartialEq, Eq)]
+pub enum AppState {
+    Start,
+    Parsing,
+    Done,
+    Exit,
+}
+
+#[derive(Debug)]
+pub struct App {
+    input_lines: Vec<String>,
+    parsed_lines: Vec<String>,
+    chunk_size: usize,
+    last_parsing_idx: usize,
+    total: u32,
+    state: AppState,
+}
+
+impl App {
+    pub fn default(input_lines: Vec<String>) -> App {
+        App {
+            input_lines,
+            state: AppState::Start,
+            parsed_lines: vec![],
+            chunk_size: 5,
+            last_parsing_idx: 0,
+            total: 0,
+        }
+    }
+
+    pub fn with_chunk_size(&mut self, chunk_size: usize) -> &mut App {
+        self.chunk_size = chunk_size;
+        self
+    }
+
+    /// runs the application's main loop until the user quits
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while self.state != AppState::Exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+            self.update();
+        }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        let timeout = Duration::from_secs_f32(1.0 / 20.0);
+        if event::poll(timeout)? {
+            if let Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press {
+                    match key.code {
+                        KeyCode::Char(' ') | KeyCode::Enter => self.start_parsing(),
+                        KeyCode::Char('q') | KeyCode::Esc => self.exit(),
+                        _ => {}
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn exit(&mut self) {
+        self.state = AppState::Exit;
+    }
+
+    fn start_parsing(&mut self) {
+        self.state = AppState::Parsing;
+    }
+
+    fn update(&mut self) {
+        if self.state == AppState::Parsing {
+            // parse chunk of lines
+            let last_idx = self.input_lines.len() - 1;
+            let next_idx = (self.last_parsing_idx + self.chunk_size).max(last_idx);
+            for line_idx in self.last_parsing_idx..next_idx {
+                // Line parsing logic goes here
+                let line_parts: Vec<&str> = self.input_lines[line_idx].split(" ").collect();
+                let result_str_parts: Vec<&str> = line_parts[0].split(":").collect();
+                let result: u32 = result_str_parts[0].parse().unwrap();
+                let mut nums: Vec<u32> = vec![];
+                for num_idx in 1..line_parts.len() {
+                    nums.push(line_parts[num_idx].parse().unwrap())
+                }
+                if is_valid(result, nums) {
+                    self.total += result;
+                }   
+            }
+            self.last_parsing_idx = self.chunk_size;
+            if next_idx == last_idx {
+                self.state = AppState::Done;
+            }
+        }
+    }
+}
+
+
+pub fn is_valid(result: u32, nums: Vec<u32>) -> bool {
+    // check if result cna be found with operations
+    
+    
+    false
+}
+
+// implement Widget trait for App so we can just call render_widget once
+// and all the render code can live here
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        match self.state {
+            AppState::Start => {
+                let title = Line::from(" Advent of Code 2024 Over Engineered Terminal UI ".bold());
+                let instructions = Line::from(vec![
+                    " Quit ".into(),
+                    "<Q> ".blue().bold(),
+                    " Parse ".into(),
+                    "<Space> ".blue().bold(),
+                ]);
+                let block = Block::bordered()
+                    .title(title.centered())
+                    .title_bottom(instructions.centered())
+                    .border_set(border::THICK);
+                let line_count = self.input_lines.len();
+                let line_count_text = Text::from(vec![Line::from(vec![
+                    "Input Line Count: ".into(),
+                    format!("{line_count}").to_string().yellow(),
+                ])]);
+
+                Paragraph::new(line_count_text)
+                    .centered()
+                    .block(block)
+                    .render(area, buf);
+            }
+            AppState::Parsing => {
+                let title = Line::from(" Advent of Code 2024 Over Engineered Terminal UI ".bold());
+                let instructions = Line::from(vec![" Quit ".into(), "<Q> ".blue().bold()]);
+                let block = Block::bordered()
+                    .title(title.centered())
+                    .title_bottom(instructions.centered())
+                    .border_set(border::THICK);
+                let progress = self.last_parsing_idx as f64 / self.input_lines.len() as f64;
+                Gauge::default()
+                    .block(block)
+                    .ratio(progress)
+                    .render(area, buf);
+            }
+            AppState::Done => {
+                let title = Line::from(" Advent of Code 2024 Over Engineered Terminal UI ".bold());
+                let instructions = Line::from(vec![" Quit ".into(), "<Q> ".blue().bold()]);
+                let block = Block::bordered()
+                    .title(title.centered())
+                    .title_bottom(instructions.centered())
+                    .border_set(border::THICK);
+                let total = self.total;
+                let line_count_text = Text::from(vec![Line::from(vec![
+                    "Total: ".into(),
+                    format!("{total}").to_string().yellow(),
+                ])]);
+
+                Paragraph::new(line_count_text)
+                    .centered()
+                    .block(block)
+                    .render(area, buf);
+            }
+            _ => {}
+        }
+    }
+}
+
+fn main() -> io::Result<()> {
+    // grab args
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
+    let lines = read_lines_from_file(file_path).unwrap();
 
-    let mut cells = vec![];
-    let mut start_idx = 0;
-    let mut start_dir = StartDir::None;
-    let mut height = 0;
-    let mut width = 0;
-    println!("start!");
-    if let Ok(lines) = read_lines_from_file(file_path) {
-        let mut idx = 0;
-        for line in lines.flatten() {
-            let line_str = line.as_str();
-            width = line_str.len();
-            for line_char in line_str.chars() {
-                match line_char {
-                    '.' => cells.push(0),
-                    '#' => cells.push(1),
-                    '^' => {
-                        cells.push(0);
-                        start_idx = idx;
-                        start_dir = StartDir::Up;
-                    }
-                    '>' => {
-                        cells.push(0);
-                        start_idx = idx;
-                        start_dir = StartDir::Right;
-                    }
-                    '<' => {
-                        cells.push(0);
-                        start_idx = idx;
-                        start_dir = StartDir::Left;
-                    }
-                    'v' => {
-                        cells.push(0);
-                        start_idx = idx;
-                        start_dir = StartDir::Down;
-                    }
-                    _ => {}
-                }
-                idx += 1;
-            }
-            height += 1;
-        }
-    }
-
-    let mut location_total = 0;
-    let cell_count = cells.len();
-    println!("total cells: {cell_count}");
-    for cell_idx in 0..cells.len() {
-        let percent_done = (cell_idx as f32 / cell_count as f32) * 100.0;
-        println!("{percent_done}%   --  count: {location_total}");
-        if cells[cell_idx] == 0 {
-            let mut new_cells = cells.clone();
-            new_cells[cell_idx] = 1;
-            let steps_to_exit = traverse_grid(&new_cells, start_idx, start_dir, width, height);
-            if steps_to_exit == 0 {
-                location_total += 1;
-            }
-        }
-    }
-    println!("spots {location_total}");
-}
-
-#[derive(Clone, Copy, PartialEq, Eq)]
-pub enum StartDir {
-    Up,
-    Down,
-    Left,
-    Right,
-    None,
-}
-
-const MAX_STEPS: u32 = 99999;
-
-pub fn traverse_grid(
-    cells: &Vec<u8>,
-    start_idx: usize,
-    start_dir: StartDir,
-    width: usize,
-    height: usize,
-) -> u32 {
-    // let mut steps = 0;
-    let mut steps_since_last_new_cell = 0;
-    let mut visited = HashSet::new();
-    let mut guard_idx = start_idx;
-    let mut guard_dir = start_dir;
-    let mut guard_action = get_guard_action(guard_idx, guard_dir, cells, width, height);
-    while guard_action != GuardAction::Exit && steps_since_last_new_cell < MAX_STEPS {
-        // steps += 1;
-        match guard_action {
-            GuardAction::Exit => {}
-            GuardAction::Turn => match guard_dir {
-                StartDir::Up => {
-                    guard_dir = StartDir::Right;
-                }
-                StartDir::Right => {
-                    guard_dir = StartDir::Down;
-                }
-                StartDir::Down => {
-                    guard_dir = StartDir::Left;
-                }
-                StartDir::Left => {
-                    guard_dir = StartDir::Up;
-                }
-                StartDir::None => {}
-            },
-            GuardAction::Advance => match guard_dir {
-                StartDir::Up => {
-                    guard_idx = guard_idx - width;
-                }
-                StartDir::Right => {
-                    guard_idx = guard_idx + 1;
-                }
-                StartDir::Down => {
-                    guard_idx = guard_idx + width;
-                }
-                StartDir::Left => {
-                    guard_idx = guard_idx - 1;
-                }
-                StartDir::None => {}
-            },
-        }
-        let current_cell_visits = visited.len();
-        visited.insert(guard_idx);
-        if current_cell_visits == visited.len() {
-            steps_since_last_new_cell += 1
-        } else {
-            steps_since_last_new_cell = 0
-        }
-        guard_action = get_guard_action(guard_idx, guard_dir, cells, width, height);
-    }
-    if steps_since_last_new_cell == MAX_STEPS {
-        0
-    } else {
-        visited.len() as u32
-    }
-}
-
-#[derive(PartialEq, Eq)]
-pub enum GuardAction {
-    Advance,
-    Exit,
-    Turn,
-}
-
-pub fn get_guard_action(
-    guard_idx: usize,
-    guard_dir: StartDir,
-    cells: &Vec<u8>,
-    width: usize,
-    height: usize,
-) -> GuardAction {
-    match guard_dir {
-        StartDir::Up => {
-            // check cell directly above
-            if guard_idx < width {
-                GuardAction::Exit
-            } else {
-                let next_cell = cells[guard_idx - width];
-                match next_cell {
-                    0 => GuardAction::Advance,
-                    1 => GuardAction::Turn,
-                    _ => GuardAction::Advance,
-                }
-            }
-        }
-        StartDir::Left => {
-            if guard_idx % width == 0 {
-                GuardAction::Exit
-            } else {
-                let next_cell = cells[guard_idx - 1];
-                match next_cell {
-                    0 => GuardAction::Advance,
-                    1 => GuardAction::Turn,
-                    _ => GuardAction::Advance,
-                }
-            }
-        }
-        StartDir::Down => {
-            if guard_idx > ((height - 1) * width) {
-                GuardAction::Exit
-            } else {
-                let next_cell = cells[guard_idx + width];
-                match next_cell {
-                    0 => GuardAction::Advance,
-                    1 => GuardAction::Turn,
-                    _ => GuardAction::Advance,
-                }
-            }
-        }
-        StartDir::Right => {
-            if guard_idx % width == (width - 1) {
-                GuardAction::Exit
-            } else {
-                let next_cell = cells[guard_idx + 1];
-                match next_cell {
-                    0 => GuardAction::Advance,
-                    1 => GuardAction::Turn,
-                    _ => GuardAction::Advance,
-                }
-            }
-        }
-        StartDir::None => GuardAction::Advance,
-    }
+    // create ratatui terminal
+    let mut terminal = ratatui::init();
+    terminal.clear()?;
+    let mut app = App::default(lines);
+    app.run(&mut terminal).unwrap();
+    // once app exits, return to normal terminal
+    ratatui::restore();
+    Ok(())
 }
